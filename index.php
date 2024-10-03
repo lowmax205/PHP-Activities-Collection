2af27e86bf45bebfc6b05b8bf028fd676c411b11
Nilo Jr. - LAPTOP <nilojr.olang@gmail.com>
2024-10-03 14:12:37 +0800

    NA

--- index.php
+++ index.php
@@ -4,11 +4,83 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Activity List</title>
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-around;
+        padding: 20px;
+    }
+
+    .column {
+        border: 2px solid black;
+        padding: 10px;
+        width: 30%;
+    }
+
+    .column h2 {
+        text-align: center;
+    }
+
+    .column ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    .column ul li {
+        margin: 10px 0;
+    }
+
+    .column ul li a {
+        text-decoration: none;
+        color: black;
+    }
+
+    .column ul li a:hover {
+        text-decoration: underline;
+    }
+    </style>
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="column">
+            <h2>Basic PHP</h2>
+            <ul>
+                <?php
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "<li><a href='basicphp/activity$i.php'>Activity #$i</a></li>";
+                }
+                ?>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>Program Flow</h2>
+            <ul>
+                <?php
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "<li><a href='programflow/activity$i.php'>Activity #$i</a></li>";
+                }
+                ?>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>Arrays</h2>
+            <ul>
+                <?php
+                for ($i = 1; $i <= 10; $i++) {
+                    echo "<li><a href='arrays/activity$i.php'>Activity #$i</a></li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
 </body>
 
 </html>
